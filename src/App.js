4e055f48651c725cba1f993b784c4ef65587f60f
dead_boolean <34863781+jenil123@users.noreply.github.com>
2021-03-14 22:20:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import { useState, useEffect } from 'react'
+import Axios from 'axios'
 function App() {
+  const [name, setName] = useState('')
+  const [age, setAge] = useState(0)
+  const [friends, setFriends] = useState([])
+  const addFriend = () => {
+    Axios.post('http://localhost:5000/addfriend', {
+      name: name,
+      age: age,
+    })
+      .then((response) => {
+        //alert('It worked')
+        setFriends([
+          ...friends,
+          { _id: response.data._id, name: name, age: age },
+        ])
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+  }
+  const updateFriend = (id) => {
+    const newAge = prompt('Enter new Age...')
+    Axios.put('http://localhost:5000/update', { age: newAge, id: id }).then(
+      () => {
+        setFriends(
+          friends.map((friend) => {
+            return friend._id === id
+              ? { _id: id, name: friend.name, age: newAge }
+              : friend
+          })
+        )
+      }
+    )
+  }
+  const deleteFriend = (id) => {
+    Axios.delete(`http://localhost:5000/delete/${id}`).then(() => {
+      setFriends(
+        friends.filter((friend) => {
+          return friend._id !== id
+        })
+      )
+    })
+  }
+  useEffect(() => {
+    Axios.get('http://localhost:5000/read')
+      .then((response) => {
+        setFriends(response.data)
+        console.log('friends Data', friends)
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='inputs'>
+        Name
+        <input
+          type='text'
+          placeholder='Enter name here...'
+          onChange={(e) => setName(e.target.value)}
+        />
+        Age
+        <input type='Number' onChange={(e) => setAge(e.target.value)} />
+        <button onClick={() => addFriend()}>Add Friend</button>
+      </div>
+      <div className='listOfFriends'>
+        {friends.map((friend) => {
+          return (
+            <div className='friendContainer'>
+              <div className='friend'>
+                <h3>Name: {friend.name}</h3>
+                <h3> Age: {friend.age}</h3>
+              </div>
+              <button onClick={() => updateFriend(friend._id)}>Update</button>
+              <button id='removeBtn' onClick={() => deleteFriend(friend._id)}>
+                Delete
+              </button>
+            </div>
+          )
+        })}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
